e9733064ef4be668ca0f75fb824545101199b584
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-10-05 13:52:47 -0400

    Fix phase 7

--- rakefile.rb
+++ rakefile.rb
@@ -8,3 +8,9 @@ Rake::TestTask.new(:test) do |t|
 end
 
 task :default => :test
+
+desc "What version of test65 is this?"
+task :vers do |t|
+  puts
+  puts "test65 version = #{::Test65::VERSION}"
+end
