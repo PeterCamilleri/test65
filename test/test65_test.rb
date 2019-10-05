d8fbf6a2e6b4ead83778f6cb5276b1314a40de47
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-10-05 13:52:47 -0400

    Fix phase 4

--- test/test65_test.rb
+++ test/test65_test.rb
@@ -1,11 +1,22 @@
-require "test_helper"
+require_relative '../lib/test65'
+require          'minitest/autorun'
+require          'minitest_visible'
 
 class Test65Test < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Test65::VERSION
+    refute_nil(::Test65::VERSION)
+    assert(::Test65::VERSION.frozen?)
+    assert(::Test65::VERSION.is_a?(String))
+    assert(/\A\d+\.\d+\.\d+/ =~ ::Test65::VERSION)
+  end
+
+  def test_that_it_has_a_description
+    refute_nil(::Test65::DESCRIPTION)
+    assert(::Test65::DESCRIPTION.frozen?)
+    assert(::Test65::DESCRIPTION.is_a?(String))
   end
 
   def test_it_does_something_useful
-    assert false
+    assert(false)
   end
 end
--- test/test_helper.rb
+++
@@ -1,4 +0,0 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "test65"
-
-require "minitest/autorun"
