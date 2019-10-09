b501a50c16c44495ca6120eb3500f411617efe4a
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-10-09 15:55:15 -0400

    Added the help option

--- lib/test65/process_args.rb
+++ lib/test65/process_args.rb
@@ -1,14 +1,17 @@
 
 module Test65
 
+  @file_list = []
+  @path = nil
+
+
   # Process the command line arguments
   def self.process_args
-
     opts = GetoptLong.new(
       [ "--help",       "-h", "-?", GetoptLong::NO_ARGUMENT ],
       [ "--version",    "-v",       GetoptLong::NO_ARGUMENT ],
       [ "--file",       "-f",       GetoptLong::REQUIRED_ARGUMENT ],
-      [ "--dir",        "-d",       GetoptLong::REQUIRED_ARGUMENT ])
+      [ "--path",       "-p",       GetoptLong::REQUIRED_ARGUMENT ])
 
     opts.each do |opt, arg|
       case opt
@@ -17,14 +20,32 @@ module Test65
       when "--version"
         display_version
       when "--file"
-        @file = arg
-      when "--dir"
-        @directory = arg
+        @file_list << arg
+      when "--path"
+        @path = arg
       else
         fail "Invalid option: #{opt}"
       end
     end
 
+  end
+
+  # Display program usage info.
+  def self.display_help
+    puts <<-EOF
+test65 - Run tests of 65c02 assembler code.
+
+Usage: test65 {options}
+
+Options:
+  --help, -h, -? Display this message and exit.
+  --version, -v  Display the program version and exit.
+  --file, -f     Specify a test file to test. (Multiple allowed)
+  --path, -p     Specify the path to test files. (Only 1 allowed)
+
+    EOF
+
+    exit
   end
 
 end
