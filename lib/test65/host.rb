6f27f8325c64dd5c3962cdba483b95edcff0dcb9
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-10-09 18:08:33 -0400

    Simplifed host detect code

--- lib/test65/host.rb
+++ lib/test65/host.rb
@@ -4,17 +4,8 @@ module Test65
 
   # Are we running under Windows?
   def self.windows?
-
-    #What operating platform is in use?
-    @host_system_type ||=
-    case RbConfig::CONFIG['host_os']
-    when /mswin|msys|mingw|bccwin|wince|emc/
-      :windows
-    else
-      :not_windows
-    end
-
-    @host_system_type == :windows
+    # Is this a Windows based system
+    @win ||= RbConfig::CONFIG['host_os'] =~ /mswin|msys|mingw|bccwin|wince|emc/
   end
 
 end
