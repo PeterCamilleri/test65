dd59e7633ebc06c534572da99c6e95a1f7be2cad
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-10-09 13:17:19 -0400

    Documented process return values in source

--- lib/test65.rb
+++ lib/test65.rb
@@ -1,7 +1,18 @@
 # test65
 
+require 'english'
 require_relative 'test65/version'
 
 module Test65
-  # Application code goes here!
+
+  # Run some 65c02 tests.
+  # Returns
+  #   0 for success
+  #   2..255 for test failure. The status is the failure code.
+  #   :asm for an assembler error
+  #   :linker for a linker error
+  #   :sim for a simulator error.
+  def self.process(args)
+  end
+
 end
