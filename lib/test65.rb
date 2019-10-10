d8baf5f71e163248963395f7464b0bacc3bf53f6
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-10-10 09:12:31 -0400

    Added code to compute the path to the gem root folder

--- lib/test65.rb
+++ lib/test65.rb
@@ -12,6 +12,10 @@ require_relative 'test65/version'
 
 module Test65
 
+  #Figure out where the gem root folder is located
+  lib_path = File.dirname(File.absolute_path(__FILE__))
+  @gem_root = Pathname.new(lib_path).parent.to_s
+
   # The code entry point. Run some 65c02 tests.
   # Returns
   #   0 for success
