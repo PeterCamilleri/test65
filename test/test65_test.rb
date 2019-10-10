45865445a610cfdfd9939f875eafa46717824867
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-10-10 13:12:46 -0400

    Starting adding some tests... FINALLY!

--- test/test65_test.rb
+++ test/test65_test.rb
@@ -3,6 +3,10 @@ require          'minitest/autorun'
 require          'minitest_visible'
 
 class Test65Test < Minitest::Test
+
+  #Track mini-test progress.
+  include MinitestVisible
+
   def test_that_it_has_a_version_number
     refute_nil(::Test65::VERSION)
     assert(::Test65::VERSION.frozen?)
@@ -14,6 +18,18 @@ class Test65Test < Minitest::Test
     refute_nil(::Test65::DESCRIPTION)
     assert(::Test65::DESCRIPTION.frozen?)
     assert(::Test65::DESCRIPTION.is_a?(String))
+  end
+
+  def test_that_help_works
+    system "ruby exe/test65 -? > foo\n"
+    assert_equal(0, $?.exitstatus)
+    system "rm foo"
+  end
+
+  def test_that_version_works
+    system "ruby exe/test65 -v > foo\n"
+    assert_equal(0, $?.exitstatus)
+    system "rm foo"
   end
 
 end
