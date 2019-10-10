e173820ce6e9bf57c516fa7b8f43fad12040bc32
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-10-10 12:50:07 -0400

    Factored out the help text

--- lib/test65/process_args.rb
+++ lib/test65/process_args.rb
@@ -14,7 +14,7 @@ module Test65
     opts.each do |opt, arg|
       case opt
       when "--help"
-        display_help
+        puts IO.read(@gem_root + "/help.txt")
         exit
       when "--debug"
         @debug = true
@@ -35,30 +35,6 @@ module Test65
     @file_list = ARGV
 
     path
-  end
-
-  # Display program usage info.
-  def self.display_help
-    puts <<-EOF
-test65 - Run tests of 65c02 assembler code.
-
-Usage: test65 {options} {files}
-
-Options:
-  --help, -h, -?   Display this message and exit.
-  --version, -v    Display the program version and exit.
-  --path, -p       Specify the path to the test files. (Only 1 allowed)
-  --debug, -d      Display lots of useful progress info.
-
-Files: An optional list of test files.
-
-Notes:
-- By default, test files are located in a folder called "t65" in the current
-  folder or one of its parent folders.
-- If no files are specified, then all files matching "t65*.a65" in the test
-  folder are tested. Otherwise only the specified file(s) are tested.
-
-    EOF
   end
 
 end
