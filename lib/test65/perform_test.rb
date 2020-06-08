9f42f6155699925956ead7aac0cd1004f13e983d
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-06-08 13:23:05 -0400

    Starting to lay groundwork

--- lib/test65/perform_test.rb
+++ lib/test65/perform_test.rb
@@ -9,8 +9,21 @@ class TestScript
   # Create a test script object and set up its default options.
   def initialize(options={}, source=nil)
     @options = options.full_dup
-    @options[:asm_src] = source.nil? ? [] : [source]
-    @options[:objs]    = []
+
+    if source
+      @options[:asm_src] = [source]
+      @options[:objs]    = [change_type(source, ".o")]
+      @options[:out]     = change_type(source, ".out")
+    else
+      @options[:asm_src] = []
+      @options[:objs]    = []
+      @options[:out]     = "a.out"
+    end
+  end
+
+  # Create a file name with a new extension.
+  def change_type(file, new_ext)
+    file.gsub(/\...?.?\z/, new_ext)
   end
 
 end
