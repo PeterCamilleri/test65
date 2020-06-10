3314b9c76e0c5509e98c2b2ec76583c87afcc1b7
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-06-10 10:35:58 -0400

    Now resets the asminc paths

--- lib/test65/enclosures/ca65.rb
+++ lib/test65/enclosures/ca65.rb
@@ -2,7 +2,13 @@
 
 class TestScript
 
-  # Assemble some files
+  # Add some include paths for the assembler.
+  def inc_paths(*more_paths)
+    fail "Sequence error" unless @phase == :create
+    (@options[:asminc] << more_paths).flatten!
+  end
+
+  # Assemble some files.
   def ca65(*more_files)
     fail "Sequence error" unless @phase == :create
 
@@ -10,7 +16,7 @@ class TestScript
     asminc  = build_args("-I", @options[:asm_src])
     @options[:asm_src] = []
     target  = "--target #{@options[:target]} "
-    quiet = @options[:quiet].to_s
+    quiet   = @options[:quiet].to_s
 
     # Convert source assemble files into object files.
     source_files.each do |source|
@@ -21,5 +27,9 @@ class TestScript
 
       @options[:objs] << object
     end
+
+    # Reset back to default paths.
+    @options[:asminc] = ["#{@options[:gem_root]}/asminc"]
   end
+
 end
