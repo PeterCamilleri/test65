209958ee15c342f4684b7d6739474d5b622376c1
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-06-09 22:19:41 -0400

    Typo and refactor

--- lib/test65/enclosures/ca65.rb
+++ lib/test65/enclosures/ca65.rb
@@ -10,12 +10,13 @@ class TestScript
     asminc  = build_args("-I", @options[:asm_src])
     @options[:asm_src] = []
     target  = "--target #{@options[:target]} "
+    quiet = @options[:quiet].to_s
 
     # Convert source assemble files into object files.
     source_files.each do |source|
       object = change_type(source, ".o")
       list   = @options[:list] ? "-l " + change_type(source, ".lst") : ""
-      system("ca65 #{target} #{@asminc} #{sources} #{lst} -o #{object} #{@options[:quiet].to_s}\n")
+      system("ca65 #{target} #{@asminc} #{sources} #{list} -o #{object} #{quiet}\n")
       fail "Error assembling #{localize_path(source)}" unless $?.exitstatus == 0
 
       @options[:objs] << object
