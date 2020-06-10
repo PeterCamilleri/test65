2189fd3a13f01850af788aefa5a3ffa35e53ebb1
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-06-09 20:19:48 -0400

    Added more to the ca65 process

--- lib/test65/enclosures/ca65.rb
+++ lib/test65/enclosures/ca65.rb
@@ -6,14 +6,19 @@ class TestScript
   def ca65(*more_files)
     fail "Sequence error" unless @phase == :create
 
-    source_files = (@options[:asm_src] + more_files).flatten
+    sources = (@options[:asm_src] + more_files).flatten
+    asminc  = build_args("-I", @options[:asm_src])
     @options[:asm_src] = []
+    target  = "--target #{@options[:target]} "
 
     # Convert source assemble files into object files.
-    source_files.each do |source_file|
+    source_files.each do |source|
+      object = change_type(source, ".o")
+      list   = @options[:list] ? "-l " + change_type(source, ".lst") : ""
+      system("ca65 #{target} #{@asminc} #{sources} #{lst} -o #{object} #{@options[:quiet].to_s}\n")
+      fail "Error assembling #{localize_path(source)}" unless $?.exitstatus == 0
 
-
-      @options[:objs] << change_type(source_file, ".o")
+      @options[:objs] << object
     end
   end
 end
