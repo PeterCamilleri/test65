01d6aee849f620eb2b2bb81c73a2d34016e0df91
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-06-14 10:02:10 -0400

    Started adding tests

--- test/test65_test.rb
+++ test/test65_test.rb
@@ -48,6 +48,10 @@ class Test65Test < Minitest::Test
   def test_that_it_succeeds
     system "ruby exe/test65 min_pass.a65 > foo\n"
     assert_equal(0, $?.exitstatus)
+
+    system "ruby exe/test65 min_pass.rb > foo\n"
+    assert_equal(0, $?.exitstatus)
+
     system "rm foo\n"
   end
 
