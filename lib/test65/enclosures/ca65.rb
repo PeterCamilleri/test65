d35568e3d11636bce7a3c161fb02e187f90e2817
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-06-15 11:46:33 -0400

    Cleanup handling of ca65 paths

--- lib/test65/enclosures/ca65.rb
+++ lib/test65/enclosures/ca65.rb
@@ -5,7 +5,6 @@ class TestScript
   def ca65_initialize
     @options[:target] = "sim65c02"
     @options[:ca65_paths] = ["#{@options[:gem_root]}/asminc"]
-    @options[:ca65_options] = []
     @options[:objs] = []
   end
 
@@ -15,32 +14,22 @@ class TestScript
     append_option(:inc_paths, more_paths)
   end
 
-  def ca65_options(*more_options)
-    fail "Sequence error: ca65_options" unless @phase == :create
-    append_option(:ca65_options, more_options)
-  end
-
   # Assemble some files.
-  def ca65(file)
+  def ca65(file, options="")
     fail "Sequence error: ca65" unless @phase == :create
     source  = @options[:path] + "/" + file
     target  = "--target #{@options[:target]} "
     paths   = build_args("-I", @options[:ca65_paths])
-    options = build_args(@options[:ca65_options])
 
     # Convert source assemble files into object files.
     object = change_type(source, ".o")
     list   = @options[:list] ? "-l " + change_type(source, ".lst") : ""
-    command = "ca65 #{target}#{paths}#{list}#{options}-o #{object} #{source} #{@quiet}\n"
+    command = "ca65 #{target}#{paths}#{list}#{options} -o #{object} #{source} #{@quiet}\n"
     puts command if @options[:debug]
     system(command)
     fail "Error assembling #{source.localize_path}" unless $?.exitstatus == 0
 
     @options[:objs] << object
-
-    # Reset path and options back to defaults.
-    @options[:ca65_paths] = ["#{@options[:gem_root]}/asminc"]
-    @options[:ca65_options] = []
   end
 
 end
