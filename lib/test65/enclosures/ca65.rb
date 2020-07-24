018ef4f6f30100304fa6115843101591481a265e
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-07-24 08:56:16 -0400

    Clarified the type of paths

--- lib/test65/enclosures/ca65.rb
+++ lib/test65/enclosures/ca65.rb
@@ -4,14 +4,14 @@ class TestScript
   # Setup ca65
   def ca65_initialize
     @options[:ca65_target] = "sim65c02"
-    @options[:ca65_paths] = ["#{@options[:gem_root]}/asminc"]
+    @options[:ca65_inc_paths] = ["#{@options[:gem_root]}/asminc"]
     @options[:objs] = []
   end
 
   # Add some include paths for the assembler.
   def ca65_inc_paths(*more_paths)
     fail "Sequence error: ca65_inc_paths" unless @phase == :create
-    append_option(:inc_paths, more_paths)
+    append_option(:ca65_inc_paths, more_paths)
   end
 
   # Assemble some files.
@@ -19,7 +19,7 @@ class TestScript
     fail "Sequence error: ca65" unless @phase == :create
     source  = File.absolute_path(@options[:path] + "/" + file)
     target  = "--target #{@options[:ca65_target]} "
-    paths   = build_args("-I", @options[:ca65_paths])
+    paths   = build_args("-I", @options[:ca65_inc_paths])
 
     # Convert source assemble files into object files.
     object = change_type(source, ".o")
